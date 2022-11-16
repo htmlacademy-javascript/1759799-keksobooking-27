affea1ad03333ca281c227c779ccb605c6d505ba
eugeneDev <smertin.ee@gmail.com>
2022-11-16 17:53:11 +0300

    второй модуль

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
 // возвращает NaN
-function randomInteger(min, max, round){
+function random(min, max, round = 3){
   if (max < 0 || min < 0 || max <= min){
     return NaN;
   }
-  let rand = Math.random() * (max - min + 1) + min;
-  return rand.toFixed(round);
+  const rand = Math.random() * (max - min) + min;
+  return +rand.toFixed(round);
 }
+random(1, 3, 3);
 
 
 // просит ввести другие числа
-function randomInteger2(min, max, round){
+function random2(min, max, round = 3){
   if (max < 0 || min < 0 || max <= min){
-    return alert("попробуйте ввести другие числа");
+    return('попробуйте ввести другие числа');
   }
-  let rand = Math.random() * (max - min + 1) + min;
-  return rand.toFixed(round);
+  const rand = Math.random() * (max - min) + min;
+  return +rand.toFixed(round);
 }
+random2(1, 3, 3);
 
 //меняет minmax местами
-function randomInteger3(min, max, round){
+function random3(min, max, round = 3){
   if (max < 0 || min < 0 || max <= min){
-    let crutchmax = min;
+    const crutchMax = min;
     min = max;
-    max = crutchmax;
-  }else if (max == min){
+    max = crutchMax;
+  }else if (max === min){
     return NaN;
   }
-  let rand = Math.random() * (max - min + 1) + min;
-  return rand.toFixed(round);
+  const rand = Math.random() * (max - min) + min;
+  return +rand.toFixed(round);
 }
+random3(1, 3, 3);
 
 {/* методы взяты из учебника https://learn.javascript.ru/ */}
